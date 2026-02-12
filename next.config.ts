d02bf23a196ca191badb315258adbb37075d9d8f
Chris Witalewski <chris.w@meetcleo.com>
2026-02-12 16:56:14 +0100

    update base path for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/cortina-2",
+  assetPrefix: "/cortina-2/",
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
